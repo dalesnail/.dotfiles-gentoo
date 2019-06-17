f32031a6aa570ef256cab388d69fc66b87e2ce70
dalesnail <cameronmills.work@gmail.com>
2019-06-17 10:41:26 -0400

    Incrimental updates

--- bash/.bash_functions/dot_push.sh
+++ bash/.bash_functions/dot_push.sh
@@ -2,5 +2,5 @@
 
 #Update dots with commit descriptions
 updot () {
-	cd ~/dotfiles && git add . && git add -u && git commit -m "$1" && git push origin master && cd -
+	cd ~/nest && git add . && git add -u && git commit -m "$1" && git push origin master && cd -
 } 
